6ff258e78ce9353f45746f8776eb62ddcb22f1fb
Selvin11 <528370630@qq.com>
2016-05-24 20:58:47 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,42 +5,37 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         //Metadata
-        meta: {
-            basePath: './',
-            srcPath: './sass/',
-            deployPath: './css/'
-        },
+        // meta: {
+        //     basePath: './',
+        //     srcPath: './sass/',
+        //     deployPath: './css/'
+        // },
         uglify: {
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
-            }
-        },
+             options: {
+               banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+             },
+             build: {
+               src: 'js/<%=pkg.name %>.js',
+               dest: 'js/<%= pkg.name %>.min.js'
+             }
+           },
         sass: {
           dist: {
             files: {
-              '<%= meta.deployPath %>selvin.css': '<%= meta.srcPath %>selvin.scss'
+              'css/selvin.css': 'sass/selvin.scss'
             },
             options: {
                sourcemap: 'true'
             }
           }
         },
-        banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' */\n',
-        // usebanner: {
-        //     dist: {
-        //         options: {
-        //             position: 'top',
-        //             banner: '<%= banner %>'
-        //         },
-        //         files: {
-        //             src: ['<%= meta.deployPath %>/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
-        //         }
-        //     }
-        // },
+        cssmin:{
+            dist:{
+                files:{
+                    'css/selvin.css': 'css/selvin.min.css'
+                }
+            }
+        },
         watch: {
             scripts: {
                 files: ['js/<%= pkg.name %>.js'],
@@ -50,17 +45,22 @@ module.exports = function(grunt) {
                 },
             },
             sass: {
-                files: ['<%= meta.srcPath %>/**/*.scss'],
+                files: ['sass/**/*.scss'],
                 tasks: ['sass']
+            },
+            cssmin:{
+                files: ['css/**/*.min.css'],
+                tasks: ['cssmin']
             }
         }
     });
 
     // 执行任务的顺序
     grunt.loadNpmTasks('grunt-sass');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 默认的执行任务
-    grunt.registerTask('default', ['sass' , 'watch']);
+    grunt.registerTask('default', ['sass'  , 'watch']);
 
 };
